e49c74b91fe139ff94e3fdca0c2c5946c07a6ab8
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-19 12:04:08 +0000

    Add post route

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const multer = require('multer');
+const upload = multer();
 
 const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
+});
+
+app.post('/upload', upload.single('file'), function(req, res) {
+  res.json({
+    size: req.file.size
+  });
 });
 
 app.listen(port, function() {
