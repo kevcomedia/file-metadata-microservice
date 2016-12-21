ac7294af1df44d3a5a2e35271529bca2f7d76190
Kev Comedia <kevcomedia@users.noreply.github.com>
2016-12-21 14:15:34 +0000

    Display response on page

--- public/form.js
+++ public/form.js
@@ -1,10 +1,23 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', function(e) {
+  const output = document.getElementById('output');
+  output.innerHTML = '';
+
   const formData = new FormData();
   const fileInput = document.getElementById('file');
   formData.append('file', fileInput.files[0]);
 
   const request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState !== request.DONE) return;
+    if (request.status !== 200) return;
+
+    const response = JSON.parse(request.responseText);
+    output.innerHTML =
+`<div class="well">
+  <samp>{<br>&nbsp;&nbsp;size: ${response.size}<br>}</samp>
+</div>`;
+  };
   request.open('POST', '/upload', true);
   request.send(formData);
 
